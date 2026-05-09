3f0195553ea1bfb51bcd4889d5e574b0999b0afc
cage433 <cage433@gmail.com>
2026-05-09 16:34:27 +0800

    jk

---
+++ lua/custom/plugins/jk.lua
@@ -0,0 +1,11 @@
+-- lua with lazy.nvim
+return {
+  { "max397574/better-escape.nvim", opts = {} },
+  {
+      'numToStr/Comment.nvim',
+      opts = {
+          -- add any options here
+      }
+  }
+
+}
